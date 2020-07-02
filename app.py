53bc43e0ad2173dca5d7a5841ba1aefb46f3fc3f
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-07-02 17:31:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 ########### Set up the layout
-from jupyter_dash import JupyterDash
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
